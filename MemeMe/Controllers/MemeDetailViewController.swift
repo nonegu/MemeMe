c0e28ee619e52b7bded90ce41d0c6acac15389b3
Ender Güzel <enderguzel@gmail.com>
2019-07-20 09:40:02 +0300

    Create Meme Detail View Controller

---
+++ MemeMe/Controllers/MemeDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  MemeMe
+//
+//  Created by Ender Güzel on 20.07.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    
+    
+    
+}
